c76d89f978816ee57bc8ab163a15f775ab3bd7ce
Fernando García <fgarcia78@gmail.com>
2020-09-30 13:18:55 -0500

    Fix

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1>Lab OpenShift</h1><p>Hola, Tepic!</p>x\r\n", 200, { 'Content-Type': 'text/html' }
+    return "<h1>Lab OpenShift</h1><p>Hola, Tepic!</p>", 200, { 'Content-Type': 'text/html' }
 
 if __name__ == '__main__':
     application.run(debug = True)
