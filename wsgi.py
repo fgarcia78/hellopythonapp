33a963ce915b33801f0a27cea1d06c4a4d7b97e0
Fernando García <fgarcia78@gmail.com>
2020-10-01 00:55:36 -0500

    Fix to test webhook

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h2>Lab OpenShift</h2><p>Hola, Tepic!</p>", 200, { 'Content-Type': 'text/html' }
+    return "<h2>Lab OpenShift</h2><p>Hola desde Tepic!</p>", 200, { 'Content-Type': 'text/html' }
 
 if __name__ == '__main__':
     application.run(debug = True)
